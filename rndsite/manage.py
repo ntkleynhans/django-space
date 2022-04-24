b9e13c357f6c3264340b7878aaef4da74f192731
Neil Kleynhans <neil@patchwork.health>
2022-04-24 13:50:27 +0200

    linting and formatting

--- rndsite/manage.py
+++ rndsite/manage.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 def main():
     """Run administrative tasks."""
     load_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rndsite.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rndsite.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,5 +20,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
